874d1c71c5b7c2119026af0323fbcf48f9f9fe47
CJ Sveningsson <cj.sveningsson@gmail.com>
2022-11-26 16:34:35 +0100

    feat [fish, powerline]: Add fish, omf and powerline conf
    Probably still need to ensure they are installed, so see:
    
    * https://fishshell.com/
    * https://github.com/oh-my-fish/oh-my-fish
    * https://gist.github.com/TrentSPalmer/63a85b582d42ab4bff665fc2dbba42e2
    
    Add vim mode since fish does not care about .inputrc, see
    https://stackoverflow.com/questions/28444740/how-to-use-vi-mode-in-fish-shell

---
+++ _config_fish/conf.d/interactive-powerline.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+	set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+	source /usr/share/powerline/bindings/fish/powerline-setup.fish
+	powerline-setup
+end
